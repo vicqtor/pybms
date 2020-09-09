a98c4cad994e81883956e3c592e8e128f7ff748d
mwangi victor <tmvicbvn@gmail.com>
2020-09-09 11:46:06 +0300

    Update models.py

--- sec_modules/models.py
+++ sec_modules/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+# password module
+def passwd_pin():
+	pass
+
+# voice_bio module
+def voice_bio():
+	pass
+
+# fprint module
+def fprint_bio():
+	pass
+
+# pprint module
+def pprint_bio():
+	pass
+
+# facial module
+def facial_bio():
+	pass
+
+# rtn module
+def rtn_bio():
+	pass
